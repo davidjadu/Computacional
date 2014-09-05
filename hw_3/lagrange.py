32db1386391401aa67a3866f530ec50c143d529d
David Jaramillo Duque <d.jaramillo1553>
2014-09-05 10:29:31 -0500

    Versión final

--- hw_3/lagrange.py
+++ hw_3/lagrange.py
@@ -1,59 +1,72 @@
 import numpy as np
 
-M_EARTH = 1
-M_SUN = 332948.6 
-ITERATIONS = 100000
+h=1e-4
+M2=1
+M1=332948.6
+c=M2/(M1+M2) #Centro de Masa
 
-mass_center = 1.0/(M_SUN + M_EARTH)*(M_EARTH)
-mu = M_EARTH*M_SUN/(M_EARTH+M_SUN)
+x=[]
+y=[]
 
-x = []
-y = []
+def newton(x0, y0,h):
+    x=x0
+    y=y0
+    while ((abs(fun1(x,y))>(1e-9) or abs(fun2(x,y))>(1e-9))):
+        dx, dy= np.linalg.solve(np.array([[derivate_x(fun1,x,y,h),derivate_y(fun1,x,y,h)],[derivate_x(fun2,x,y,h),derivate_y(fun2,x,y,h)]]), np.array([-fun1(x,y),-fun2(x,y)]))
+        x+=dx
+        y+=dy
+    return x,y
 
-def newton(func,func_prime, inicio):
-    sol = inicio
-    delta = 0
-    for n in range(ITERATIONS):
-        if (func(sol)==0):
-            break
-        else :
-            delta = - func(sol)/func_prime(sol)
-            sol += delta
-    return sol
-
-def f_lagrange_1(r):
-    return M_SUN/r**2 - M_EARTH/(1.0-r)**2 - (r-mass_center)*(M_EARTH+M_SUN)
-
-def f_lagrange_1_prime(r):
-    return -2*M_SUN/r**3 - 2*M_EARTH/(1.0-r)**3 - (M_EARTH+M_SUN)
+def fun1(x,y):
+    return M1/(x**2+y**2)*((x*(c-x)-y**2)/(np.sqrt(x**2+y**2)))+M2/((x-1)**2+y**2)*(((x-1)*(c-x)-y**2)/(np.sqrt((1-x)**2+y**2)))+(M1+M2)*((x-c)**2+y**2)
     
-y.append(0) #L1 is clearly in y=0
-x.append(newton(f_lagrange_1, f_lagrange_1_prime, 0.8))
+def fun2(x,y):
+    return M1/(x**2+y**2)*((c*y)/(np.sqrt(x**2+y**2)))+M2/((1-x)**2+y**2)*((c*y-y)/(np.sqrt((1-x)**2+y**2)))
+    
+def derivate_x(f,x,y,h):
+    return 1/(2.0*h)*(f(x+h,y)-f(x-h,y))
 
-def f_lagrange_2(r):
-    return M_SUN/r**2 + M_EARTH/(1.0-r)**2 - (r-mass_center)*(M_EARTH+M_SUN)
-
-def f_lagrange_2_prime(r):
-    return -2*M_SUN/r**3 + 2*M_EARTH/(1.0-r)**3 - (M_EARTH+M_SUN)
-
-y.append(0) #L2 is clearly in y=0
-x.append(newton(f_lagrange_2, f_lagrange_2_prime, 1.1))
-
-def f_lagrange_3(r):
-    return M_SUN/r**2 + M_EARTH/(1.0-r)**2 - (M_EARTH+M_SUN)*(mass_center -r)
-
-def f_lagrange_3_prime(r):
-    return -2*M_SUN/r**3 + 2*M_EARTH/(-r+1.0)**3 + (M_EARTH+M_SUN)
-
-y.append(0) #L3 is clearly in y=0
-x.append(newton(f_lagrange_3, f_lagrange_3_prime, -0.9))
-
-x.append(0.5) #L4 is clearly in x=0.5
+def derivate_y(f,x,y,h):
+    return 1/(2.0*h)*(f(x,y+h)-f(x,y-h))
 
 
+"""    
+Un cuerpo que que orbite apenas mas cerca del sol que la tierra tendra una velocidad angular apenas mas grande que la tierra; pero si se encuentra la tierra al otro lado, se tendra que esta reducira levemente esta velocidad haciendola un poco mas lento. Por tanto se espera encontrar un punto entre la tierra y el sol (L1)
+"""
+x_actual, y_actual = newton(0.5,0,h)
+x.append(x_actual)
+y.append(y_actual)
 
-x.append(0.5) #L5 is clearly in x=0.5
-y.append(-np.sqrt(1-0.25)) #Pitagoras theorem
+"""
+Un cuerpo que que orbite apenas mas lejos del sol que la tierra tendra una velocidad angular apenas mas pequena que la tierra; pero si se encuentra la tierra al mismo lado del sol, se tendra que asta aumentara levemente esta velocidad haciandola un poco mayor. Por tanto se espera encontrar un punto en la linea que une el sol y la tierra mas alla de esta ultima (L2)
+"""
+x_actual, y_actual = newton(1.5,0,h)
+x.append(x_actual)
+y.append(y_actual)
+
+"""
+Un cueerpo que orbite a poco mas que la distancia entre el sol y la tierra tendra una velocidad apenas poco menos que la velocidad angular de la tierra. Pero si la la tierra se encuentra completamnete al otro lado del sol, entonces aumentara un poco la velocidad. Por tanto se espera encontrar un punto cerca de alli (L3)
+"""
+x_actual, y_actual = newton(-1.0,0,h)
+x.append(x_actual)
+y.append(y_actual)
+
+"""
+Si un cuerpo se encuentra sobre la linea x=0.5 se tiene que debido a que la fuerza gravitacional es proporcional a la masa, asi como el centro de masa del sistema es lineal con la masa, la fuerza ira dirigida al centro de masa. sobre otras lineas, la fuerza no va dirigida al centro de masa, por tanto no pueden haber mas soluciones. Ademas como el problema es totalmente simetrico sobre el eje y se esperan dos soluciones sobre esto linea (L4,L5)
+"""
+x_actual, y_actual = newton(0.5,1,h)
+x.append(x_actual)
+y.append(y_actual)
+
+x_actual, y_actual = newton(0.5,-1,h)
+x.append(x_actual)
+y.append(y_actual)
+
+"""
+Por el analisis realizado no se esperan encontrar mas puntos, De igual manera si se ponen otros numeros iniciales, siempre el metodo converge a alguno de los puntos anteriores
+Nota: No hay tildes, porque el programa no me compila con tildes
+"""
+
 
 fileout = open("puntos_lagrange.dat","w")
 for i in range(len(x)):
