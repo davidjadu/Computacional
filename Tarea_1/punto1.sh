6a734cef30e89472ce97fc8592e30fe932e24518
David Jaramillo Duque <d.jaramillo1553>
2014-09-10 16:22:25 -0500

    Eliminando Carpeta repetida (hw_1)

--- Tarea_1/punto1.sh
+++
@@ -1,7 +0,0 @@
-wget https://raw.githubusercontent.com/forero/ComputationalMethodsData/master/hands_on/solar/monthrg.dat
-echo El promedio de manchas solares en 11/1993 fue de:
-awk < monthrg.dat '{if($1==1993 && $2==11) print $4}'
-echo La cantidad de meses desde 1900 hasta 1950 que tuvieron más de 30 manchas solares fueron:
-awk < monthrg.dat '{if($1<1951 && $1>1899 && $4>30) print $4}' | wc -l
-echo El año y el mes con más manchas solares en toda la histroia fue:
-sort -k 4n monthrg.dat | tail -n 1 | awk '{print $1,$2}'
--- Tarea_1/punto2.sh
+++
@@ -1,8 +0,0 @@
-wget https://raw.githubusercontent.com/forero/ComputationalMethodsData/master/homework/hw_1/notas_fisicaII_201320.dat
-echo Las cantidad de personas que perdieron exactamente un parcial y ganaron la materia fue:
-awk < notas_fisicaII_201320.dat '{if(($6>=60)&&(($1<60 && $2>=60 && $3>=60)||($1>=60 && $2<60 && $3>=60)||($1>=60 && $2>=60 && $3<60))) print $1,$2,$3,$6}'|wc -l
-echo El mejor promedio en parciales de las personas que pasaron la materia fue:
-awk < notas_fisicaII_201320.dat '{if($6>=60) print ($1+$2+$3)/3}'|sort -n | tail -n 1
-echo El peor promedio en parciales de las personas que pasaron la materia fue:
-awk < notas_fisicaII_201320.dat '{if($6>=60) print ($1+$2+$3)/3}'|sort -n | head -n 1
-
